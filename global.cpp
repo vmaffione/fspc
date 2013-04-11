9a37fe97b62f51402ac1ddc15886e37daad454cb
Vincenzo Maffione <v.maffione@gmail.com>
2013-04-11 16:52:02 +0200

    global.cpp filled

--- global.cpp
+++ global.cpp
@@ -56,3 +56,103 @@ void Aliases::insert(const string& left, const string& right) {
 	groups[index].push_back(AliasElement(right, false));
 }
 
+void Aliases::fill_process_table(SymbolsTable& pt)
+{
+    for (int i=0; i<groups.size(); i++) {
+	int found = 0;
+	int index = -1;
+	SymbolValue * svp;
+	ProcessValue * pvp;
+
+	/* For each set, we have to find the unique process (if any) that
+	   is already in 'pt', e.g. a process that has been defined as a
+	   proper process (with a non-NULL ProcessNode*) and not just as
+	   an alias. */
+	for (int j=0; j<groups[i].size(); j++) {
+	    if (pt.lookup(groups[i][j].name, svp)) {
+		found++;
+		index = j;
+	    } else if (!groups[i][j].assigned) {
+		/* We make sure that every non-properly-defined process
+		   has been assigned somewhere as an alias.*/
+		stringstream errstream;
+		errstream << "$* Process " << groups[i][j].name
+		    << " undefined";
+		semantic_error(errstream);
+	    }
+	}
+	assert(found <= 1);
+	if (found == 0) {
+	    cerr << "Warning, aliases cycle found: {";
+	    for (int j=0; j<groups[i].size(); j++)
+		cerr << groups[i][j].name << ", ";
+	    cerr << "}\n";
+	    cerr << "    A new STOP process will be associated to these aliases\n";
+	    pvp = new ProcessValue;
+	    pvp->pnp = new ProcessNode;
+	    svp = pvp;
+	    pt.insert(groups[i][0].name, svp);
+	    index = 0;
+	}
+	pvp = err_if_not_process(svp);
+	for (int j=0; j<groups[i].size(); j++)
+	    if (j != index) {
+		ProcessValue * npvp = new ProcessValue;
+
+		npvp->pnp = pvp->pnp;
+		if (!pt.insert(groups[i][j].name, npvp)) {
+		    /* This really can't happen. */
+		    stringstream errstream;
+		    errstream << "Impossible duplicate (BUG)\n";
+		    semantic_error(errstream);
+		}
+		cout << "Process " << groups[i][j].name << " defined (" << npvp->pnp << ")\n";
+	    }
+    }
+}
+
+void Aliases::clear()
+{
+    groups.clear();
+}
+
+void Aliases::print()
+{
+    cout << "Aliases:\n";
+    for (int i=0; i<groups.size(); i++)
+	for (int j=0; j<groups[i].size(); j++)
+	    cout << i << ": " << groups[i][j].name << ", "
+		<< groups[i][j].assigned << "\n";
+}
+
+void Global::init_fakenode() {
+    vector<ProcessEdge> cv;
+    struct ProcessEdge e;
+    struct FrontierElement fe;
+    vector<FrontierElement> frontier;
+
+    /* The fakenode must have a child for each contexts. In this way the 
+       actions associated to the rule 'prefix_actions-->action_labels' can
+       correctly expand each context and return a ProcessNode* for each
+       child. */
+    fe.pnp = &fakenode;
+    for (int c=0; c<current_contexts().size(); c++) {
+	e.dest = NULL;
+	e.rank = c;	/* The child will combine with the c-th context. */
+	e.action = 0;	/* Not significant. */
+	cv.push_back(e);
+	/* Put the child in the frontier. */
+	fe.child = c;
+	frontier.push_back(fe);
+    }
+    fakenode.children = cv;
+    current_contexts().frontier = frontier;
+}
+
+void Global::print_fakenode_forest() {
+    cout << "Current ProcessNode fakenode forest:\n";
+    for (int i=0; i<fakenode.children.size(); i++)
+	if (fakenode.children[i].dest)
+	    fakenode.children[i].dest->print(&actions);
+}
+
--- global.hpp
+++ global.hpp
@@ -1,7 +1,6 @@
 #ifndef __GLOBAL__HH
 #define __GLOBAL__HH
 
-#include <cstdio>
 #include <iostream>
 #include <sstream>
 #include <cstdlib>
@@ -13,15 +12,8 @@ using namespace std;
 //#define NDEBUG
 #include <assert.h>
 
-/* Stuff from flex that bison needs to know about. */
-#include "scanner.hpp"
-
-/* Lts definitions and operations. */
-#include "lts.hpp"
-
-/* Context, ContextsSet and ContextsSetStack. */
 #include "context.hpp"
-
+#include "symbols_table.hpp"
 #include "utils.hpp"
 
 
@@ -50,70 +42,9 @@ struct Aliases {
        'pt'. The translator will call this function when it wants to move
        the aliases collected during the translation into a process symbols
        table. */
-    void fill_process_table(SymbolsTable& pt) {
-	for (int i=0; i<groups.size(); i++) {
-	    int found = 0;
-	    int index = -1;
-	    SymbolValue * svp;
-	    ProcessValue * pvp;
-
-	    /* For each set, we have to find the unique process (if any) that
-	       is already in 'pt', e.g. a process that has been defined as a
-	       proper process (with a non-NULL ProcessNode*) and not just as
-	       an alias. */
-	    for (int j=0; j<groups[i].size(); j++) {
-		if (pt.lookup(groups[i][j].name, svp)) {
-		    found++;
-		    index = j;
-		} else if (!groups[i][j].assigned) {
-		    /* We make sure that every non-properly-defined process
-		       has been assigned somewhere as an alias.*/
-		    stringstream errstream;
-errstream << "$* Process " << groups[i][j].name
-			<< " undefined";
-		    semantic_error(errstream);
-		}
-	    }
-	    assert(found <= 1);
-	    if (found == 0) {
-		cerr << "Warning, aliases cycle found: {";
-		for (int j=0; j<groups[i].size(); j++)
-		    cerr << groups[i][j].name << ", ";
-		cerr << "}\n";
-		cerr << "    A new STOP process will be associated to these aliases\n";
-		pvp = new ProcessValue;
-		pvp->pnp = new ProcessNode;
-		svp = pvp;
-		pt.insert(groups[i][0].name, svp);
-		index = 0;
-	    }
-	    pvp = err_if_not_process(svp);
-	    for (int j=0; j<groups[i].size(); j++)
-		if (j != index) {
-		    ProcessValue * npvp = new ProcessValue;
-
-		    npvp->pnp = pvp->pnp;
-		    if (!pt.insert(groups[i][j].name, npvp)) {
-			/* This really can't happen. */
-			stringstream errstream;
-errstream << "Impossible duplicate (BUG)\n";
-			semantic_error(errstream);
-		    }
-		    cout << "Process " << groups[i][j].name << " defined (" << npvp->pnp << ")\n";
-		}
-	}
-    }
-
-    void clear() {
-	groups.clear();
-    }
-
-    void print() {
-	cout << "Aliases:\n";
-	for (int i=0; i<groups.size(); i++)
-	    for (int j=0; j<groups[i].size(); j++)
-		cout << i << ": " << groups[i][j].name << ", " << groups[i][j].assigned << "\n";
-    }
+    void fill_process_table(SymbolsTable& pt);
+    void clear();
+    void print();
 };
 
 struct Global {
@@ -147,36 +78,9 @@ struct Global {
 
     ContextsSet& current_contexts() { return css.top(); }
 
-    void init_fakenode() {
-	vector<ProcessEdge> cv;
-	struct ProcessEdge e;
-	struct FrontierElement fe;
-	vector<FrontierElement> frontier;
+    void init_fakenode();
 
-	/* The fakenode must have a child for each contexts. In this way the 
-	   actions associated to the rule 'prefix_actions-->action_labels' can
-	   correctly expand each context and return a ProcessNode* for each
-	   child. */
-	fe.pnp = &fakenode;
-	for (int c=0; c<current_contexts().size(); c++) {
-	    e.dest = NULL;
-	    e.rank = c;	/* The child will combine with the c-th context. */
-	    e.action = 0;	/* Not significant. */
-	    cv.push_back(e);
-	    /* Put the child in the frontier. */
-	    fe.child = c;
-	    frontier.push_back(fe);
-	}
-	fakenode.children = cv;
-	current_contexts().frontier = frontier;
-    }
-
-    void print_forest() {
-	cout << "Current ProcessNode fakenode forest:\n";
-	for (int i=0; i<fakenode.children.size(); i++)
-	    if (fakenode.children[i].dest)
-		fakenode.children[i].dest->print(&actions);
-    }
+    void print_fakenode_forest();
 };
 
 #endif
