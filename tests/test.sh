6a197cd8d6f3a45594355b38988fb1d1dbe10f64
Vincenzo Maffione <v.maffione@gmail.com>
2013-04-25 18:58:28 +0200

    composited if then else checked.. ok

--- tests/test.sh
+++ tests/test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-for i in {1..19}
+for i in {1..20}
 do
     ./fspc -i tests/input${i}.fsp -o tests/new-output${i}.lts
     diff tests/output${i}.lts tests/new-output${i}.lts > /dev/null
