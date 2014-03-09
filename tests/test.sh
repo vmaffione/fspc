a084c70680a70e4a4cc39c781fdca3c48ad712a6
Vincenzo Maffione <v.maffione@gmail.com>
2014-03-09 10:11:22 +0100

    reorganize testing and add minimization tests

--- tests/test.sh
+++ tests/test.sh
@@ -9,50 +9,80 @@ else
 fi
 
 ##################### tests on correct input ##################
+TESTDIR="tests/blackbox"
 for i in {1..29}
 do
-    if [ ! -f "tests/input${i}.fsp" ]; then
-	echo "error: tests/input${i}.fsp not found"
+    if [ ! -f "${TESTDIR}/input${i}.fsp" ]; then
+	echo "error: ${TESTDIR}/input${i}.fsp not found"
 	exit 255
     fi
-    ${FSPC} -i tests/input${i}.fsp -o tests/new-output${i}.lts
-    diff tests/output${i}.lts tests/new-output${i}.lts > /dev/null
+    ${FSPC} -i ${TESTDIR}/input${i}.fsp -o ${TESTDIR}/new-output${i}.lts
+    diff ${TESTDIR}/output${i}.lts ${TESTDIR}/new-output${i}.lts > /dev/null
     var=$?
     if [ "$var" != "0" ]; then
 	echo ""
-	echo "Test FAILED on input${i}.fsp"
+	echo "Test FAILED on ${TESTDIR}/input${i}.fsp"
 	exit 1
     fi
-    ${FSPC} -l tests/new-output${i}.lts -o /dev/null
+    ${FSPC} -l ${TESTDIR}/new-output${i}.lts -o /dev/null
     var=$?
     if [ "$var" != "0" ]; then
         echo ""
-        echo "Test FAILED on input${i}.fsp, while reopening the compiled output."
+        echo "Test FAILED on ${TESTDIR}/input${i}.fsp, while reopening the compiled output."
         exit 1
     fi
-    rm tests/new-output${i}.lts
-    echo "input$i ok"
+    rm ${TESTDIR}/new-output${i}.lts
+    echo "${TESTDIR}/input$i ok"
 done
 
 
 ##################### tests on invalid input ##################
+TESTDIR="tests/error"
 for i in {1..2}
 do
-    if [ ! -f "tests/err-input${i}.fsp" ]; then
-	echo "error: tests/err-input${i}.fsp not found"
+    if [ ! -f "${TESTDIR}/err-input${i}.fsp" ]; then
+	echo "error: ${TESTDIR}/err-input${i}.fsp not found"
 	exit 255
     fi
-    ${FSPC} -i tests/err-input${i}.fsp -o tests/new-output.lts &> /dev/null
+    ${FSPC} -i ${TESTDIR}/err-input${i}.fsp -o ${TESTDIR}/new-output.lts &> /dev/null
     var=$?
     if [ "$var" == "0" ]; then
 	echo ""
-	echo "Test FAILED on err-input${i}.fsp"
+	echo "Test FAILED on ${TESTDIR}/err-input${i}.fsp"
 	exit 1
     fi
-    if [ -f "tests/new-output.lts" ]; then
-	rm tests/new-output.lts
+    if [ -f "${TESTDIR}/new-output.lts" ]; then
+	rm ${TESTDIR}/new-output.lts
     fi
-    echo "err-input$i ok"
+    echo "${TESTDIR}/err-input$i ok"
+done
+
+
+##################### tests with scripts #####################
+TESTDIR="tests/scripts"
+for i in {1..1}
+do
+    if [ ! -f "${TESTDIR}/input${i}.fsp" ]; then
+	echo "error: ${TESTDIR}/input${i}.fsp not found"
+	exit 255
+    fi
+    ${FSPC} -i ${TESTDIR}/input${i}.fsp -S ${TESTDIR}/script${i}.fsh -o ${TESTDIR}/new-output${i}.lts
+    diff ${TESTDIR}/output${i}.lts ${TESTDIR}/new-output${i}.lts > /dev/null
+    var=$?
+    if [ "$var" != "0" ]; then
+	echo ""
+	echo "Test FAILED on ${TESTDIR}/input${i}.fsp"
+	exit 1
+    fi
+    ${FSPC} -l ${TESTDIR}/new-output${i}.lts -o /dev/null
+    var=$?
+    if [ "$var" != "0" ]; then
+        echo ""
+        echo "Test FAILED on ${TESTDIR}/input${i}.fsp, while reopening the compiled output."
+        exit 1
+    fi
+    rm ${TESTDIR}/new-output${i}.lts
+    echo "${TESTDIR}/input$i ok"
 done
 
 
